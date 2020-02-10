0216e11e8cbe5f42a902cd1c054f2f8081b510fd
wbrink <will.brink55@gmail.com>
2020-02-10 14:33:30 -0800

    gets temps and humidities for forecast

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,16 @@ var curWind = $("#wind");
 var curUV = $("#uv");
 var curUVRange = $("#uv-range");
 
+// variables for forecast
+var tomorrow;
+var secondDay;
+var thirdDay;
+var fourthDay;
+var fifthDay;
+
+// fills in the next 5 days
+setDate();
+
 // when form is submitted
 form.on("submit", search);
 
@@ -80,6 +90,9 @@ function getCurrentWeather(url) {
   });
 }
 
+// makes sure it local time 
+// order of importance snow, rain,clouds, clear
+// sort temps for each day
 function getForecast(url) {
   $.ajax({
     url: url,
@@ -89,44 +102,192 @@ function getForecast(url) {
     console.log("error");
   })
   .then(function(res) {
-    var shiftSeconds = res.city.timezone;
+
+    // need to account for UTC Time
+    //var shiftSeconds = res.city.timezone;
+    
+    var clear1 = 0;
+    var clouds1 = 0;
+    var rain1 = 0;
+    var snow1 = 0
+    var temps1 = [];
+    var humidities1 = [];
+
+    var clear2 = 0;
+    var clouds2 = 0;
+    var rain2 = 0;
+    var snow2 = 0;
+    var temps2 = [];
+    var humidities2 = [];
+
+    var clear3 = 0;
+    var clouds3 = 0;
+    var rain3 = 0;
+    var snow3 = 0;
+    var temps3 = [];
+    var humidities3 = [];
+
+    var clear4 = 0;
+    var clouds4 = 0;
+    var rain4 = 0;
+    var snow4 = 0;
+    var tempHigh5 = 0;
+    var temps4 = [];
+    var humidities4 = [];
+
+    var clear5 = 0;
+    var clouds5 = 0;
+    var rain5 = 0;
+    var snow5 = 0;
+    var temps5 = [];
+    var humidities5 = [];
+
+
+    for (var i = 0; i < res.list.length; i++) {
+      localTime = res.list[i].dt;
+      localTime = new Date(localTime * 1000); // converts to local time
+      
+      // tomorrow
+      if (localTime >= tomorrow  && localTime < secondDay ) {
+        if (res.list[i].weather.main === "Clear") {
+          clear1++;
+        } else if (res.list[i].weather.main === "Clouds") {
+          clouds1++;
+        } else if (res.list[i].weather.main === "Rain") {
+          rain1++; 
+        }
+        else if (res.list[i].weather.main === "Snow") {
+          snow1++; 
+        }
+        temps1.push(res.list[i].main.temp)
+        humidities1.push(res.list[i].main.humidity);
+
+      }
+      // secondDay
+      else if (localTime >= secondDay  && localTime < thirdDay) {
+        if (res.list[i].weather.main === "Clear") {
+          clear1++;
+        } else if (res.list[i].weather.main === "Clouds") {
+          clouds1++;
+        } else if (res.list[i].weather.main === "Rain") {
+          rain1++; 
+        }
+        else if (res.list[i].weather.main === "Snow") {
+          snow1++; 
+        }
+
+        temps2.push(res.list[i].main.temp)
+        humidities2.push(res.list[i].main.humidity);
+      }
+      // third
+      else if (localTime >= thirdDay  && localTime < fourthDay) {
+        if (res.list[i].weather.main === "Clear") {
+          clear1++;
+        } else if (res.list[i].weather.main === "Clouds") {
+          clouds1++;
+        } else if (res.list[i].weather.main === "Rain") {
+          rain1++; 
+        }
+        else if (res.list[i].weather.main === "Snow") {
+          snow1++; 
+        }
+
+        temps3.push(res.list[i].main.temp)
+        humidities3.push(res.list[i].main.humidity);
+      }
+      // fourth
+      else if (localTime >= fourthDay  && localTime < fifthDay) {
+        if (res.list[i].weather.main === "Clear") {
+          clear1++;
+        } else if (res.list[i].weather.main === "Clouds") {
+          clouds1++;
+        } else if (res.list[i].weather.main === "Rain") {
+          rain1++; 
+        }
+        else if (res.list[i].weather.main === "Snow") {
+          snow1++; 
+        }
+
+        temps4.push(res.list[i].main.temp)
+        humidities4.push(res.list[i].main.humidity);
+      }
+      // fifth
+      else if (localTime >= fifthDay) {
+        if (res.list[i].weather.main === "Clear") {
+          clear1++;
+        } else if (res.list[i].weather.main === "Clouds") {
+          clouds1++;
+        } else if (res.list[i].weather.main === "Rain") {
+          rain1++; 
+        }
+        else if (res.list[i].weather.main === "Snow") {
+          snow1++; 
+        }
+
+        temps5.push(res.list[i].main.temp)
+        humidities5.push(res.list[i].main.humidity);
+
+      }
+    } // end for
+  
+    // sort arrays
+    $(".highTemp1").text(`${temps1.sort(function(a,b) {return b-a})[0]} F`);
+    $(".lowTemp1").text(`${temps1.sort()[0]} F`);
+    $(".humidity1").text(`${humidities1.sort()[humidities1.length-1]}`);
+
+    $(".highTemp2").text(`${temps2.sort(function(a,b) {return b-a})[0]} F`);
+    $(".lowTemp2").text(`${temps2.sort()[0]} F`);
+    $(".humidity2").text(`${humidities2.sort()[humidities2.length-1]}`);
+
+    $(".highTemp3").text(`${temps3.sort(function(a,b) {return b-a})[0]} F`);
+    $(".lowTemp3").text(`${temps3.sort()[0]} F`);
+    $(".humidity3").text(`${humidities3.sort()[humidities3.length-1]}`);
+
+    $(".highTemp4").text(`${temps4.sort(function(a,b) {return b-a})[0]} F`);
+    $(".lowTemp4").text(`${temps4.sort()[0]} F`);
+    $(".humidity4").text(`${humidities4.sort()[humidities4.length-1]}`);
+
+
+    $(".highTemp5").text(`${temps5.sort(function(a,b) {return b-a})[0]} F`);
+    $(".lowTemp5").text(`${temps5.sort()[0]} F`);
+    $(".humidity5").text(`${humidities5.sort()[humidities5.length-1]}`);
+
+    // function for figuring out which icon will be present
+
+    
+  });
+}
+
+// sets the forecast dates when the page loads
+function setDate() {
     tomorrow = new Date(); //gives local time
     tomorrow.setDate(tomorrow.getDate() + 1)
     tomorrow.setHours(0,0,0,0); // sets date to tomorrow 12AM add offset
+    $(".date1").text(`${tomorrow.getMonth()+1}/${tomorrow.getDate()}/${tomorrow.getFullYear()}`);
     
     secondDay = new Date(); //gives local time
     secondDay.setDate(secondDay.getDate() + 2)
     secondDay.setHours(0,0,0,0); // sets date to tomorrow 12AM add offset
+    $(".date2").text(`${secondDay.getMonth()+1}/${secondDay.getDate()}/${secondDay.getFullYear()}`);
 
     thirdDay = new Date(); //gives local time
     thirdDay.setDate(thirdDay.getDate() + 3)
     thirdDay.setHours(0,0,0,0); // sets date to tomorrow 12AM add offset
+    $(".date3").text(`${thirdDay.getMonth()+1}/${thirdDay.getDate()}/${thirdDay.getFullYear()}`);
 
     fourthDay = new Date(); //gives local time
-    fourthDay.setDate(fourthDay.getDate() + 3)
+    fourthDay.setDate(fourthDay.getDate() + 4)
     fourthDay.setHours(0,0,0,0); // sets date to tomorrow 12AM add offset
+    $(".date4").text(`${fourthDay.getMonth()+1}/${fourthDay.getDate()}/${fourthDay.getFullYear()}`);
 
     fifthDay = new Date(); //gives local time
-    fifthDay.setDate(fifthDay.getDate() + 3)
+    fifthDay.setDate(fifthDay.getDate() + 5)
     fifthDay.setHours(0,0,0,0); // sets date to tomorrow 12AM add offset
+    $(".date5").text(`${fifthDay.getMonth()+1}/${fifthDay.getDate()}/${fifthDay.getFullYear()}`);
+}
 
-    var count = 0;
-    var clear = 0;
-    var clouds = 0;
-    var rain = 0;
-    for (var i = 0; i < res.list.length; i++) {
-      localTime = res.list[i].dt - shiftSeconds;
-      localTime = localTime * 1000;
-      
-      // tomorrow
-      if (localTime >= tomorrow  && localTime < secondDay ) {
-        console.log("before tomorrow")
-      }
-      // secondDay
-      else if (localTime >= secondDay  && localTime < thirdDay) {
-        counter = 1;
-      }
-    }
-    
-  });
+// function used to figure out which icon will be used for the forecast days
+function weatherIcon() {
+  
+
 }
